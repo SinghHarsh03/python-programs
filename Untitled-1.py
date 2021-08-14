274c123c4ab6227d3867bf4cbe73c7a626136963
SinghHarsh03 <88923718+SinghHarsh03@users.noreply.github.com>
2021-08-14 12:59:15 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+a=5
+li=[3,4,5,7,9]
+flag=0
+for i in range(len(li)):
+    if li[i]==a:
+        flag=flag+1
+    
+if flag>0:
+    print("element found")
+else:
+    print("not found")
+print("hii ")
+        
+    
+    
